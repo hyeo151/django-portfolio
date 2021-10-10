e72f3a2c95d7c6db97e9e51916c7560775c47d2e
Shawn Yeo <khyeo151@gmail.com>
2021-10-10 15:15:39 +1300

    Display a Single Project

--- projects/views.py
+++ projects/views.py
@@ -3,5 +3,11 @@ from projects.models import Project
 # Create your views here.
 def all_projects(request):
     projects = Project.objects.all()
-    print(projects)
     return render(request, 'projects/all_projects.html', {'projects': projects})
+
+def project_detail(request,pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, 'projects/detail.html',
+                  {'project': project})
+
+
